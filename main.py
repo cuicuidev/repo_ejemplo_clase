abdfd2ee77322208920debac4c3af724b43276b1
Dmitry <dmitry.ryzhenkov@hackaboss.com>
2025-01-29 10:38:37 -0800

    funcion para hacer histogramas

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def read_csv(path):
     return pd.read_csv(path, encoding="utf-8")
+
+def plot_df(df, col):
+    sns.histplot(df, x=col)
+    plt.show()
 
 def main():
     st.dataframe(sns.load_dataset("iris"))
