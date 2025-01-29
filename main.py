238fbbdc3e740c44aafe3d2eeb70d92804ada73c
Dmitry <dmitry.ryzhenkov@hackaboss.com>
2025-01-29 10:37:06 -0800

    he añadido read_csv

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import seaborn as sns
+import pandas as pd
+
+def read_csv(path):
+    return pd.read_csv(path, encoding="utf-8")
 
 def main():
     st.dataframe(sns.load_dataset("iris"))
